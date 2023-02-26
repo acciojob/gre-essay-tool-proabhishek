5b71035fe6889ae1e34a12e09cf6114067bfc850
Abhishek <28715657+proabhishek@users.noreply.github.com>
2023-02-26 13:37:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,9 +6,10 @@ const h3Element = document.querySelector("#wordCount");
 function handleInput(e) {
     const sentence = e.target.value;
 	
-	const match = sentence.match(/\S+/ig).length;
+	const match = sentence.match(/\S+/ig);
+	const count = match.lenght ? match.length : 0; 
 	//alert(match);
-	h3Element.textContent = match;
+	h3Element.textContent = count;
 }
 textElement.addEventListener('input', handleInput );
 
